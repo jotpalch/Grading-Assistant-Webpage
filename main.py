759704a9cae68ecea1e50901f1937e1a0c73b810
jotpalch <jotp076315217@gmail.com>
2024-06-08 01:39:52 +0800

    Fix: fail to load the icon

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     projects = [ i.replace(".json", "") for i in os.listdir('json') if i.endswith('.json') ]
-    return render_template('index.html', projects=projects.sort())
+    projects.sort()
+    return render_template('index.html', projects=projects)
 
 @app.route('/<project_name>')
 def project(project_name):
+    if not os.path.isfile(f'json/{project_name}.json'):
+        return ""
     with open(f'json/{project_name}.json', 'r') as f:
         data = json.load(f)
     return render_template('lab.html', title=f"{project_name}", data=data)
